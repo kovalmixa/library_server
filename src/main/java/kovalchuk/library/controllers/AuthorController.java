1b10fb12fcebfb836130de6fae8a1ca3f0336f45
kovalmixa <kovalchukmixa@gmail.com>
2025-05-27 13:20:08 +0300

    added api/ to rest controllers to filter them if user is unauthorised

--- src/main/java/kovalchuk/library/controllers/AuthorController.java
+++ src/main/java/kovalchuk/library/controllers/AuthorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/authors")
+@RequestMapping("/api/authors")
 public class AuthorController {
 
     private final AuthorService authorService;
--- src/main/java/kovalchuk/library/controllers/BorrowingController.java
+++ src/main/java/kovalchuk/library/controllers/BorrowingController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/borrowings")
+@RequestMapping("/api/borrowings")
 public class BorrowingController {
     private BorrowingService borrowingService;
 
